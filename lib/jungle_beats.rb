e240701d453ffe1281d0c511070a0df89c158a46
Kris Foss <kris.foss@gmail.com>
2016-03-13 15:35:33 -0600

    base expectations and one extension

--- lib/jungle_beats.rb
+++ lib/jungle_beats.rb
@@ -1,58 +1,142 @@
-require 'pry'
 require './lib/node'
-require './lib/list'
+require 'pry'
 
 class JungleBeats
-  attr_accessor :list
-  def initialize(beats)
-    @list = List.new(beats)
-    @accepted = "tee dee deep bop boop la na"
+  attr_reader :head, :next_node, :data
+  def initialize(data)
+    @head = Node.new(data)
+    @accepted = "tee dee deep beep bop boop la na"
+    @beats = []
+  end
+
+  def play
+    'say -r 500 -v Boing #{all}'
   end
 
   def validate(beats)
     @proceed = beats.split.select do |beat|
       @accepted.include?(beats)
       end
+      if @proceed.count < 1
+        ""
+      else
       @proceed.join(" ")
+    end
   end
 
-  # def processor(validated)
-  #   validate(validated)
-  # end
+  def tail
+    current_node = @head
+      until current_node.next_node.nil?
+        current_node = current_node.next_node
+      end
+      current_node
+  end
 
-  def append(beats)
-    validated = validate(beats)
-    # binding.pry
-    ready = validated.join
-    @list.append(ready)
-    # @list
+  def split_beats(beat_chunk)
+    beat_chunk.split.each do |beat_chunk|
+      @beats << beat_chunk
+    end
+    @beats
+  end
+
+  def append(data)
+    new_beats = validate(data)
+    if @head.nil?
+      @head = Node.new(new_beats)
+    else
+      tail.next_node = Node.new(new_beats)
+    end
   end
 
   def all
-    @list.all
-  end
-
-  def prepend(beats)
-    @list.prepend(beats)
-  end
-
-  def insert(position, beats)
-    @list.insert(position, beats)
-  end
-
-  def includes?(beats)
-    @list.includes?(beats)
-  end
-
-  def pop(quantity)
-    @list.pop(quantity)
+    current_node = @head
+    until current_node.nil?
+      @beats << current_node.data
+      current_node = current_node.next_node
+    end
+    @beats.join(" ").strip
   end
 
   def count
-    @list.count
+    current_node = @head
+    iterator_count = 1
+    until current_node.next_node.nil?
+      iterator_count += 1
+      current_node = current_node.next_node
+    end
+    iterator_count
+  end
+
+  def prepend(data)
+    new_beats = validate(data)
+    new_node = Node.new(new_beats)
+    new_node.next_node = @head
+    @head = new_node
+    split_beats(new_beats).count
+  end
+
+  def include?(data)
+    current_node = @head
+    while current_node.data != data
+      current_node = current_node.next_node
+      if current_node.data == data
+        return true
+      else
+        return false
+      end
+    end
+  end
+
+  def insert(number, data)
+    if validate(data) == ""
+      all
+    else
+    new_beats = validate(data).strip
+    i = number - 1
+    current_node = @head
+    i.times do
+    current_node = current_node.next_node
+      end
+    temp_node = current_node.next_node
+    current_node.next_node = Node.new(new_beats)
+    current_node.next_node.next_node = temp_node
+    all
+    end
+  end
+
+  def delete_one
+  current_node = @head
+    until current_node.next_node.next_node.nil?
+      current_node = current_node.next_node
+    end
+    temp_node = current_node.next_node
+    current_node.next_node = nil
+    temp_node.data
+  end
+
+  def pop(number)
+    if number > count - 1
+        "you are trying to pop more nodes than the list contains"
+    else
+      popped = []
+      number.times do
+        popped << delete_one
+      end
+      popped.reverse.join(" ")
+    end
   end
 
   def find(number, quantity)
-    @list.find(number, quantity)
+    @string = ""
+    current_node = @head
+    number.times do
+        current_node = current_node.next_node
+      end
+    @result = ""
+    quantity.times do
+      current_node = current_node.next_node
+      @result += ("#{current_node.data}"+ " ")
+      end
+    @result.strip
   end
 end
--- test/jungle_beats_test.rb
+++ test/jungle_beats_test.rb
@@ -1,30 +1,145 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/node'
-require './lib/list'
 require './lib/jungle_beats'
+require './lib/node'
 
 class JungleBeatsTest < Minitest::Test
-  def test_a_new_instance_of_jungle_beats_can_be_created
-    beats = JungleBeats.new("beat")
-
-    assert_kind_of JungleBeats, beats
+  def test_a_new_instance_of_JungleBeats_can_be_created
+    new_list = JungleBeats.new("start")
+    assert_equal JungleBeats, new_list.class
   end
 
-  def test_it_can_append_beats_to_a_list
-    beats = JungleBeats.new("beat")
-    beats.append("deep")
-    assert_equal "beat deep", beats.all
+  def test_head_is_start_node_when_list_is_created
+    new_list = JungleBeats.new("start")
+    assert_equal "start", new_list.head.data
+  end
+
+  def test_it_can_find_the_end_of_the_list_called_tail
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("beep")
+    assert_equal "beep", new_list.tail.data
+  end
+
+  def test_it_can_list_all
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+    assert_equal "start tee deep dee", new_list.all
+  end
+
+  def test_it_can_count_nodes_in_list
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+    assert_equal 4, new_list.count
+  end
+
+  def test_it_can_append_data_onto_the_end_of_the_list
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    assert_equal "start tee", new_list.all
+  end
+
+  def test_it_can_split_beats_into_an_array
+    new_list = JungleBeats.new("start")
+    assert_equal ["tee", "deep", "dee"], new_list.split_beats("tee deep dee")
+  end
+
+  def test_it_can_prepend_data_onto_the_start_of_the_list
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+    assert_equal 1, new_list.prepend("bop")
+    assert_equal "bop", new_list.head.data
+  end
+
+  def test_it_can_insert_tee_or_more_elements_at_an_arbitrary_position_in_the_list
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+
+    assert_equal "start tee deep bop dee", new_list.insert(3, "bop")
+  end
+
+  def test_it_can_search_for_a_word_with_includes?
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+
+    assert_equal true, new_list.include?("tee")
+    assert_equal false, new_list.include?("nope")
+  end
+
+  def test_it_can_pop_off_a_number_of_elements_from_the_list
+    # skip
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+    new_list.append("bop")
+    new_list.append("la")
+    new_list.append("na")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+    new_list.append("bop")
+    new_list.append("la")
+    new_list.append("na")
+    assert_equal "na", new_list.pop(1)
+    assert_equal "bop la", new_list.pop(2)
+    assert_equal "you are trying to pop more nodes than the list contains", new_list.pop(12)
+  end
+
+  def test_it_can_find_one_or_more_elements_based_on_arbitrary_positions_in_the_list
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+    new_list.append("bop")
+    new_list.append("la")
+    new_list.append("na")
+
+    assert_equal "bop la", new_list.find(3, 2)
+    assert_equal "deep dee bop", new_list.find(1, 3)
+    assert_equal "deep dee bop la na", new_list.find(1, 5)
   end
 
   def test_it_can_validate_and_append_beats_to_a_list
     beats = JungleBeats.new("beat")
-    beats.append("new")
-    assert_equal "beat", beats.all
+    beats.append("bop")
+    assert_equal "beat bop", beats.all
+  end
 
+  def test_it_wont_append_beats_that_dont_pass_vailidation
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+    new_list.append("nope")
+    assert_equal "start tee deep dee", new_list.all
+  end
+
+  def test_it_wont_prepend_beats_that_dont_pass_vailidation
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+    new_list.prepend("nope")
+    assert_equal "start tee deep dee", new_list.all
+  end
+
+  def test_it_wont_insert_beats_that_dont_pass_vailidation
+    new_list = JungleBeats.new("start")
+    new_list.append("tee")
+    new_list.append("deep")
+    new_list.append("dee")
+
+    assert_equal "start tee deep dee", new_list.insert(1, "nope")
   end
 end
-
-#
-# beats.append("deep")
-# assert_equal "beat deep", beats.all
